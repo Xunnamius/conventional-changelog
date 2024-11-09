da47d782004cc4145433cde72cb27b5501a2eb60
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-11-09 08:40:14 -0800

    fix: use type-fest to fix unexpected type invariance

--- index.d.ts
+++ index.d.ts
@@ -8,39 +8,52 @@
  * @typedef {import('conventional-changelog-core').Options} XchangelogOptions
  */
 /**
- * @typedef {import('conventional-changelog-writer').CommitKnownProps & import('conventional-commits-parser').Commit & { scope?: string | null, subject?: string | null }} XchangelogCommit
+ * @typedef {Omit<
+ *   import('type-fest').OmitIndexSignature<
+ *     import('conventional-changelog-writer').CommitKnownProps &
+ *       import('conventional-commits-parser').Commit & {
+ *         scope?: string | null;
+ *         subject?: string | null;
+ *         shortHash?: string | null;
+ *       }
+ *   >,
+ *   'revert'
+ * > & { revert?: Record<string, string | null | undefined> | null }} XchangelogCommit
  */
 /**
  * @typedef {XchangelogSpec & XchangelogConfigOptions & {
- * options: XchangelogOptions;
- * parserOpts: import('conventional-commits-parser').ParserOptions;
- * writerOpts: Omit<import('conventional-changelog-writer').Options<XchangelogCommit>, 'commitsSort'> & { commitsSort?: string[] }
+ *   options: XchangelogOptions;
+ *   parserOpts: import('conventional-commits-parser').ParserOptions;
+ *   writerOpts: Omit<import('conventional-changelog-writer').Options<XchangelogCommit>, 'commitsSort'> & { commitsSort?: string[] }
  * }} XchangelogConfig
  */
 /**
  * This function is a patched version of `conventionalChangelog` and works
  * identically.
  *
- * @param {import('conventional-changelog-core').Options & { cwd ?: string }} [options]
+ * @param {import('conventional-changelog-core').Options<XchangelogCommit> & { cwd ?: string }} [options]
  * @param {import('conventional-changelog-core').Context} [context]
  * @param {import('conventional-changelog-core').GitRawCommitsOptions}
  * [gitRawCommitsOpts]
  * @param {import('conventional-changelog-core').ParserOptions} [parserOpts]
- * @param {import('conventional-changelog-core').WriterOptions & { includeDetails?: boolean }} [writerOpts]
+ * @param {import('conventional-changelog-core').WriterOptions<XchangelogCommit> & { includeDetails?: boolean }} [writerOpts]
  * @param {Record<string, unknown>} [gitRawExecOpts]
  * @returns {Readable}
  */
-export default function conventionalChangelog(options?: (import("conventional-changelog-core").Options<import("conventional-commits-parser").Commit, import("conventional-changelog-writer").Context<import("conventional-changelog-writer").CommitKnownProps>> & {
+export default function conventionalChangelog(options?: (import("conventional-changelog-core").Options<XchangelogCommit, import("conventional-changelog-writer").Context<import("conventional-changelog-writer").CommitKnownProps>> & {
     cwd?: string;
-}) | undefined, context?: import("conventional-changelog-core").Context | undefined, gitRawCommitsOpts?: import("conventional-changelog-core").GitRawCommitsOptions | undefined, parserOpts?: import("conventional-changelog-core").ParserOptions | undefined, writerOpts?: (import("conventional-changelog-core").WriterOptions<import("conventional-commits-parser").Commit, import("conventional-changelog-writer").Context<import("conventional-changelog-writer").CommitKnownProps>> & {
+}) | undefined, context?: import("conventional-changelog-core").Context | undefined, gitRawCommitsOpts?: import("conventional-changelog-core").GitRawCommitsOptions | undefined, parserOpts?: import("conventional-changelog-core").ParserOptions | undefined, writerOpts?: (import("conventional-changelog-core").WriterOptions<XchangelogCommit, import("conventional-changelog-writer").Context<import("conventional-changelog-writer").CommitKnownProps>> & {
     includeDetails?: boolean;
 }) | undefined, gitRawExecOpts?: Record<string, unknown> | undefined): Readable;
 export type XchangelogSpec = import("conventional-changelog-config-spec").Config;
 export type XchangelogConfigOptions = import("conventional-changelog-core").Options.Config.Object;
 export type XchangelogOptions = import("conventional-changelog-core").Options;
-export type XchangelogCommit = import("conventional-changelog-writer").CommitKnownProps & import("conventional-commits-parser").Commit & {
+export type XchangelogCommit = Omit<import("type-fest").OmitIndexSignature<import("conventional-changelog-writer").CommitKnownProps & import("conventional-commits-parser").Commit & {
     scope?: string | null;
     subject?: string | null;
+    shortHash?: string | null;
+}>, "revert"> & {
+    revert?: Record<string, string | null | undefined> | null;
 };
 export type XchangelogConfig = XchangelogSpec & XchangelogConfigOptions & {
     options: XchangelogOptions;
--- index.js
+++ index.js
@@ -19,14 +19,24 @@ import mergeConfig from './lib/merge-config.js'
  */
 
 /**
- * @typedef {import('conventional-changelog-writer').CommitKnownProps & import('conventional-commits-parser').Commit & { scope?: string | null, subject?: string | null }} XchangelogCommit
+ * @typedef {Omit<
+ *   import('type-fest').OmitIndexSignature<
+ *     import('conventional-changelog-writer').CommitKnownProps &
+ *       import('conventional-commits-parser').Commit & {
+ *         scope?: string | null;
+ *         subject?: string | null;
+ *         shortHash?: string | null;
+ *       }
+ *   >,
+ *   'revert'
+ * > & { revert?: Record<string, string | null | undefined> | null }} XchangelogCommit
  */
 
 /**
  * @typedef {XchangelogSpec & XchangelogConfigOptions & {
- * options: XchangelogOptions;
- * parserOpts: import('conventional-commits-parser').ParserOptions;
- * writerOpts: Omit<import('conventional-changelog-writer').Options<XchangelogCommit>, 'commitsSort'> & { commitsSort?: string[] }
+ *   options: XchangelogOptions;
+ *   parserOpts: import('conventional-commits-parser').ParserOptions;
+ *   writerOpts: Omit<import('conventional-changelog-writer').Options<XchangelogCommit>, 'commitsSort'> & { commitsSort?: string[] }
  * }} XchangelogConfig
  */
 
@@ -34,12 +44,12 @@ import mergeConfig from './lib/merge-config.js'
  * This function is a patched version of `conventionalChangelog` and works
  * identically.
  *
- * @param {import('conventional-changelog-core').Options & { cwd ?: string }} [options]
+ * @param {import('conventional-changelog-core').Options<XchangelogCommit> & { cwd ?: string }} [options]
  * @param {import('conventional-changelog-core').Context} [context]
  * @param {import('conventional-changelog-core').GitRawCommitsOptions}
  * [gitRawCommitsOpts]
  * @param {import('conventional-changelog-core').ParserOptions} [parserOpts]
- * @param {import('conventional-changelog-core').WriterOptions & { includeDetails?: boolean }} [writerOpts]
+ * @param {import('conventional-changelog-core').WriterOptions<XchangelogCommit> & { includeDetails?: boolean }} [writerOpts]
  * @param {Record<string, unknown>} [gitRawExecOpts]
  * @returns {Readable}
  */
